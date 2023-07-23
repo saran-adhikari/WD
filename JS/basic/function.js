0c195e8485b35c3b4e3c3a29146c41bf820a7e95
Saran Adhikari <saranadhikari02@gmail.com>
2023-07-23 10:22:34 +0545

    init

---
+++ JS/basic/function.js
@@ -0,0 +1,6 @@
+function check(){
+
+// var get_value=document.getElementsById('tx').innerHTML;
+var get_value=document.getElementsByClassName('tx')[0].innerHTML = "Amsterdam, Nepal";
+console.log(get_value);
+}
---
+++ JS/basic/questions.js
@@ -0,0 +1,71 @@
+// Question-1
+
+// var nep=prompt("Enter the currency in Nepalese rupee: ")
+// var usd= 131.5
+// var exchange=parseFloat(nep)/parseFloat(usd);
+// document.write("Your currency in USD is: "+ exchange)
+
+// Question-2
+
+// var a=5;
+// console.log(a)
+// for(i=0;i<=3;i++){
+//     var a=a*10+5;
+//     console.log(a)
+    
+// }
+
+// Question-3
+
+// var input=prompt("Enter any number")
+// document.write("The divisors are: " + "<br>")
+// for(i=0;i<=input;i++){
+//     if(input%i==0){
+//         document.write( i + "<br>")
+//     }
+// }
+
+// Question-4
+
+// var input=prompt("Enter any number")
+// var int=parseInt(input)
+// for(i=2;i<int;i++){
+//     if(int%i===0){
+//         document.write("It is a composite number")
+//         break
+//     }
+//     else{
+//         document.write("It is a prime number")
+//         break
+//     }
+// }
+
+// Question-5
+
+// const array=[31,25,3,6,16,5,10,8,17,20]
+// var sum=0
+// var avg=0
+// const largest=Math.max(...array)
+// const smallest=Math.min(...array)
+// var oddCount=0
+// var evenCount=0
+// for(i=0;i<=array.length-1;i++){
+//     sum=parseInt(sum)+array[i]
+//     avg=sum/array.length 
+//     if(array[i]%2==0){
+//     oddCount++
+//     }
+//     else{
+//         evenCount++
+//     }
+    
+// }
+// console.log("Sum is: "+ sum)
+// console.log("Average is: " + avg)
+// console.log("Largest num is: " + largest)
+// console.log("Smallest num is: " + smallest)
+// console.log("Total odd count is: "+ oddCount)
+// console.log("Total even count is: "+ evenCount)
+
+
+
