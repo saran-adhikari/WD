4b2eff0b8e69b3a779a8ba02c59ff3e46dd2e924
Saran Adhikari <saranadhikari02@gmail.com>
2023-07-26 10:49:46 +0545

    init

--- JS/project/script.js
+++ JS/project/script.js
@@ -1,4 +1,4 @@
-function allChanges(){
+function allChanges() {
     // document.querySelector('#heading').style.color='red';
     // document.querySelector('#heading').style.backgroundColor='yellow';
     // document.querySelector('#heading').style.fontWeight='bold';
@@ -9,41 +9,58 @@ function allChanges(){
     document.querySelector('#heading').classList.add('text');
 
 }
-function change(){
-    var name=document.querySelector('#change').innerText;
-    if(name=='Hide'){
-        document.querySelector('#change').innerText='Show'
+function change() {
+    var name = document.querySelector('#change').innerText;
+    if (name == 'Hide') {
+        document.querySelector('#change').innerText = 'Show'
     }
-    else{
-        document.querySelector('#change').innerText='Hide'
+    else {
+        document.querySelector('#change').innerText = 'Hide'
     }
     document.querySelector('#heading').classList.toggle('show-hide').innerText
 }
-function color(){
-    var color=document.querySelector('#color').value;
-    document.querySelector('#second').style.color=color;
+function color() {
+    var color = document.querySelector('#color').value;
+    document.querySelector('#second').style.color = color;
 }
-function bgcolor(){
-    var bgcolor=document.querySelector('#bg').value;
-    document.querySelector('#second').style.backgroundColor=bgcolor;
+function bgcolor() {
+    var bgcolor = document.querySelector('#bg').value;
+    document.querySelector('#second').style.backgroundColor = bgcolor;
 }
-function font(){
-    var size=document.querySelector('#font').value;
-    document.querySelector('#second').style.fontSize=size
+function font() {
+    var size = document.querySelector('#font').value;
+    document.querySelector('#second').style.fontSize = size
 }
-function options(){
-    var opt=document.querySelector('#option').value
-    document.querySelector('#second').style.font=opt
-}
-function changeLight(){
-    var name=document.querySelector('#light').innerText;
-    document.querySelector('#one')[0].src='https://prabhatmallik.com.np/dom/image/ON.jpg'
+function multiple(){
+    var opt = document.querySelector('#option1').value
+    console.log(opt)
+    if(opt=='all'){
+        document.querySelector('#second').classList.add('optn')
 
-    if(name=='ON'){
-        document.querySelector('#light').innerText='OFF'
     }
-    else{
-        document.querySelector('#light').innerText='ON'
+    else if( opt=='bold'){
+        document.querySelector('#second').style.fontWeight=opt
+    }
+    else if(opt=='italic'){
+        document.querySelector('#second').style.fontStyle=opt
+    }
+    else if(opt=='underline'){
+        document.querySelector('#second').style.textDecoration=opt
+    }
+    
+}
+function changeLight() {
+    var name = document.querySelector('#light').innerText;
+    
+    if (name == 'ON') {
+        document.querySelector('#light').innerText = 'OFF'
+        document.querySelector('#one').src = 'https://prabhatmallik.com.np/dom/image/ON.jpg'
+
+    }
+    else {
+        document.querySelector('#light').innerText = 'ON'
+        document.querySelector('#one').src = 'https://prabhatmallik.com.np/dom/image/OFF.jpg'
+
     }
     document.querySelector('#light').value
 }
